b9f7ef710f1781ddfe069cf873eca68be061796e
Ilya <milyutin.id@gmail.com>
2021-12-09 12:54:42 +0300

    implemented recsys.core.model.Pipeline for working with Callables

--- recsys/core/model.py
+++ recsys/core/model.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod
-from typing import Any, Iterable
+from copy import copy
+from typing import Any, Iterable, Tuple, Callable
 from recsys.core.data import (AbstractDataTransformer,
                               IdentityDataTransformer)
 
@@ -11,6 +12,38 @@ class AbstractModel(metaclass=ABCMeta):
     @abstractmethod
     def infer(self, data: Any) -> Iterable[str]:
         pass
+
+
+class Pipeline:
+    def __init__(self, *steps: Tuple[str, Callable]):
+        self._pipeline = []
+        for step_num, step in enumerate(steps):
+            if len(step) == 0:
+                raise AttributeError(
+                    f'Step #{step_num + 1} is empty!'
+                )
+            if len(step) != 2:
+                raise AttributeError(
+                    'Each step must be of length 2'
+                    ' and match a form (<step_name>, <step_class>)'
+                )
+            self._pipeline.append({'step_name': step[0],
+                                   'step_func': step[1]})
+
+    @property
+    def schema(self):
+        print('Pipeline schema:')
+        for num, step in enumerate(self._pipeline):
+            name, func = step.values()
+            print(f'{num + 1}. Name: {name:<5}',
+                  f'Transformer: {func}', sep='\n   ')
+
+    def compose(self, data):
+        result = copy(data)
+        for step in self._pipeline:
+            step_func = step['step_func']
+            result = step_func(result)
+        return result
 
 
 class Model:
