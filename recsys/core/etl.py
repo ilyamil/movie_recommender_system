d95afae9abdddceb9653bc088b455027b9fd0dd5
Ilya <milyutin.id@gmail.com>
2021-12-10 00:35:04 +0300

    added test for recsys.etl.split_aggregate_rating_col

--- recsys/core/etl.py
+++ recsys/core/etl.py
@@ -1,5 +1,6 @@
 from typing import Optional, Dict, List
 import pandas as pd
+import numpy as np
 
 
 def split_helpfulness_col(df_raw: pd.DataFrame) -> pd.DataFrame:
@@ -7,13 +8,10 @@ def split_helpfulness_col(df_raw: pd.DataFrame) -> pd.DataFrame:
     Split 'helpfulness' column of input dataframe into two
     distinct columns: 'upvotes' and 'total_votes'.
     After transformation 'helpfulness' column is removed.
-
-    Args:
-        df_raw (pd.DataFrame): input data with column.
-
-    Returns:
-        pd.DataFrame: Output data frame.
     """
+    if 'helpfulness' not in df_raw.columns:
+        raise ValueError('No "helpfulness" column in input data')
+
     df_ = df_raw.copy(deep=False)
     df_[['upvotes', 'total_votes']] = (
         df_['helpfulness']
@@ -27,9 +25,53 @@ def split_helpfulness_col(df_raw: pd.DataFrame) -> pd.DataFrame:
 
 
 def convert_to_date(df_raw: pd.DataFrame) -> pd.DataFrame:
+    """
+    Convert 'date' column of type 'object' of input data to 'review_date'
+    column of type datetime64[ns]. After transformation the 'date' column
+    is removed.
+    """
+    if 'date' not in df_raw.columns:
+        raise ValueError('No "date" column in input data')
+
     df_ = df_raw.copy(deep=False)
     df_['review_date'] = pd.to_datetime(df_['date'])
     return df_.drop(columns=['date'])
+
+
+def expand_short_form(string_num: str) -> int:
+    short_forms = {
+        'K': 1_000,
+        'M': 1_000_000,
+        'B': 1_000_000_000,
+        'T': 1_000_000_000_000
+    }
+    last_char = string_num[-1]
+    if last_char not in short_forms.keys():
+        return float(string_num)
+    return float(string_num[:-1]) * short_forms.get(last_char, None)
+
+
+def split_aggregate_rating_col(df_raw: pd.DataFrame) -> pd.DataFrame:
+    """
+    Split column 'agg_rating' of type string into two columns:
+    'movie_rating' of type float and 'movie_total_votes' of type int.
+    After transformation the 'agg_rating' column is removed.
+    """
+    if 'agg_rating' not in df_raw.columns:
+        raise ValueError('No "agg_rating" column in input data')
+
+    df_ = df_raw.copy(deep=False)
+    df_[['rating', 'total_votes']] = (
+        df_['agg_rating']
+        .str.split('/10', expand=True)
+        .values
+    )
+    df_['total_votes'] = df_['total_votes'].apply(expand_short_form)
+    return (
+        df_
+        .astype({'rating': np.float32, 'total_votes': np.int32})
+        .drop('agg_rating', axis=1)
+    )
 
 
 details_sections = [
@@ -64,3 +106,5 @@ def extract_substrings_after_anchors(s: str, anchors: List[str])\
             details[start] = s[left_loc + len(start):]
     details.update(**dict.fromkeys(empty_anchors))
     return details
+
+
--- tests/core/test_etl.py
+++ tests/core/test_etl.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 from datetime import datetime
-from recsys.core import preprocessing
+from recsys.core import etl
 
 
 @pytest.fixture
@@ -21,11 +21,22 @@ def reviews():
     return pd.DataFrame(data)
 
 
-def test_split_helpfulness_col(reviews):
-    df_ = preprocessing.split_helpfulness_col(reviews)
+@pytest.fixture
+def movie_details():
+    data = [
+        {
+            'title_id': 1,
+            'agg_rating': '8.9/1099M'
+        }
+    ]
+    return pd.DataFrame(data)
 
-    assert 'helpfulness' not in df_.columns
+
+def test_split_helpfulness_col(reviews):
+    df_ = etl.split_helpfulness_col(reviews)
+
     assert ('upvotes' in df_.columns) and ('total_votes' in df_.columns)
+    assert 'helpfulness' not in df_.columns
     assert (df_.loc[0, 'upvotes'] == 171
             and df_.loc[0, 'total_votes'] == 185)
     assert (df_.loc[1, 'upvotes'] == 1710
@@ -33,7 +44,7 @@ def test_split_helpfulness_col(reviews):
 
 
 def test_convert_to_date(reviews):
-    df_ = preprocessing.convert_to_date(reviews)
+    df_ = etl.convert_to_date(reviews)
 
     assert 'review_date' in df_.columns
     assert 'date' not in df_.columns
@@ -43,7 +54,7 @@ def test_convert_to_date(reviews):
 def test_extract_substrings_after_anchors_right_number_of_anchors():
     s = 'anchor1 some string anchor2 another string'
     anchors = ['anchor1', 'anchor2']
-    substrings = preprocessing.extract_substrings_after_anchors(
+    substrings = etl.extract_substrings_after_anchors(
         s, anchors
     )
     assert all(anchor in substrings.keys() for anchor in anchors)
@@ -52,7 +63,7 @@ def test_extract_substrings_after_anchors_right_number_of_anchors():
 def test_extract_substrings_after_anchors_greater_number_of_anchors():
     s = 'anchor1 some string anchor2 another string'
     anchors = ['anchor1', 'anchor2', 'anchor3']
-    substrings = preprocessing.extract_substrings_after_anchors(
+    substrings = etl.extract_substrings_after_anchors(
         s, anchors
     )
     assert all(anchor in substrings.keys() for anchor in anchors)
@@ -62,8 +73,18 @@ def test_extract_substrings_after_anchors_greater_number_of_anchors():
 def test_extract_substrings_after_anchors_no_anchors_in_string():
     s = 'some string another string'
     anchors = ['anchor1', 'anchor2', 'anchor3']
-    substrings = preprocessing.extract_substrings_after_anchors(
+    substrings = etl.extract_substrings_after_anchors(
         s, anchors
     )
     assert all(anchor in substrings.keys() for anchor in anchors)
     assert all(substrings[anchor] is None for anchor in anchors)
+
+
+def test_split_aggregate_rating_col(movie_details):
+    df_ = etl.split_aggregate_rating_col(movie_details)
+
+    assert ('rating' in df_.columns)\
+           and ('total_votes' in df_.columns)
+    assert 'agg_rating' not in df_.columns
+    assert abs(df_['rating'][0] - 8.9) < 0.0001
+    assert df_['total_votes'][0] == 99_000_000
